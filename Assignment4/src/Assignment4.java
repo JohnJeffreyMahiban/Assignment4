2bd23c21c71abfa06043bac3305cc2815e1d18b4
JohnJeffreyMahiban <johnjeffrey.mahiban@gmail.com>
2016-02-12 18:23:20 +0530

    Assignment4

---
+++ Assignment4/src/Assignment4.java
@@ -0,0 +1,11 @@
+
+public class Assignment4 {
+	public static void main(String[] args) {
+			// Assignment 4
+		int  x=5,y=10;
+		System.out.println(x + y * 2);
+		System.out.println(x - y + 2);
+		System.out.println((x+y) * 2);
+		System.out.println(y % x);
+                }
+}
